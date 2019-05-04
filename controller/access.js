9f8dcec11956fe341b7d03b6db36b86806b9ebd0
stevewooo <461437874@qq.com>
2019-05-04 22:33:47 +0800

    规范一下入口

--- controller/access.js
+++ controller/access.js
@@ -9,12 +9,6 @@ module.exports = async (swc, options)=>{
 	});
 	swc = await swc.registerStatic(swc, {
 		items : [{
-			path : '/' + swc.config.server.bussiness_name + '/res',
-			staticFilePath : `./keke_stage/static/res`
-		} , {
-			path : '/' + swc.config.server.bussiness_name + '/admin',
-			staticFilePath : `./keke_stage/static/admin`
-		}, {
 			path : `/${swc.config.server.bussiness_name}/public`,
 			staticFilePath : `${path.resolve()}/public`
 		}]
